a802f84b099df824c76e9fddd8b3f81658936ed5
CheeNae <jo6876@naver.com>
2020-12-04 16:01:56 +0900

    cloudwebproject

--- src/Components/TopmenuData.js
+++ src/Components/TopmenuData.js
@@ -3,13 +3,14 @@ import * as AiIcons from 'react-icons/ai';
 
 export const TopmenuData = [
     {
-        title: 'Notice',
-        icon: <AiIcons.AiOutlineBell />,
-        path: '/notice',
-    },
-    {
         title: 'Signin',
         icon:  <AiIcons.AiOutlineUser />,
         path: '/signin',
+    },
+    {
+        title: 'Notice',
+        icon: <AiIcons.AiOutlineBell />,
+        path: '/notice',
     }
+
 ]
